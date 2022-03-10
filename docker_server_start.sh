bb42e55c74ff6777a92a1f0058479888b36ae82b
Szymon Zimnowoda <szymon.zimnowoda@gmail.com>
2022-03-10 17:12:49 +0100

    fix correct paths for mounting, fix docker under linux

--- docker_server_start.sh
+++ docker_server_start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x # echo on
 #`getent hosts test-vm-server | awk '{ print $$1 }'` is resolving current ip address
 /ya-vm-file-server/target/release/ya-vm-file-server --mount-point server_root_fs --network-address `getent hosts test-vm-server | awk '{ print $1 }'`:7878
 
