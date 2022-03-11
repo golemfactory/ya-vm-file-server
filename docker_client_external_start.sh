82c8ecf51f8f1b5212bd8edfd013a034ed899fbf
scx1332 <sieciech.czajka@golem.network>
2022-03-11 16:47:21 +0100

    Modified external test

--- docker_client_external_start.sh
+++ docker_client_external_start.sh
@@ -1,15 +1,18 @@
-nohup socat TCP4-LISTEN:7100 TCP4-LISTEN:12398 &
+sudo nohup socat TCP4-LISTEN:7100 TCP4-LISTEN:12398 &
 echo "Sleeping 5 seconds before starting"
 sleep 5s
-cd /ya-vm-file-server
+cd /home/dock/ya-vm-file-server
+MOUNT_POINT="./mnt_tests"
 echo "Mounting filesystem from external server"
-echo mount -t 9p -o version=9p2000.L,trans=tcp,debug=0x04,port=7100,uname=testuser 127.0.0.1 ./mnt_tests/
-mount -t 9p -o version=9p2000.L,trans=tcp,debug=0x04,port=7100,uname=testuser 127.0.0.1 ./mnt_tests/
+echo sudo mount -t 9p -o version=9p2000.L,trans=tcp,debug=0x04,port=7100,uname=testuser 127.0.0.1 ./mnt_tests/
+sudo mount -t 9p -o version=9p2000.L,trans=tcp,debug=0x04,port=7100,uname=testuser 127.0.0.1 ./mnt_tests/
 echo "Confirm mount"
 if mountpoint -q -- "mnt_tests"; then
   printf '%s\n' "client_fs is a mount point"
   ls -la ./mnt_tests
   printf '%s\n' "running tests"
   cargo +nightly test
-  sleep 1000s
+
 fi
+
+sleep 1000s
